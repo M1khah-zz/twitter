4d1d816f1619044e124dd48ed6b0dc82fa8a6895
Kirill <alexeyenko92@gmail.com>
2017-11-28 11:00:45 +0000

    rspec & tests added

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+    class StaticPagesController < ApplicationController
+        def home
+            @title = 'Twitter'
+        end
+    end
+end
