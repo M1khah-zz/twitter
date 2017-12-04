78fc90b60cab757d34565cd463791ae0207adf41
Kirill <alexeyenko92@gmail.com>
2017-12-04 14:40:29 +0000

    added basic layout for registration & overwritten devise controllers partly

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.first_name }
     surname { Faker::Name.last_name }
-    nickname { Faker::Internet.user_name(3..13) }
+    nickname { Faker::Internet.user_name(3..20) }
   end
 end
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -5,14 +5,17 @@ feature 'Registration' do
   
   scenario 'Visitor registers successfully via register form' do
     visit new_user_registration_path
+    expect(page).to have_content 'Sign up'
     within '#new_user' do
       fill_in 'Email', with: Faker::Internet.email
+      fill_in 'Name', with: Faker::Name.first_name
+      fill_in 'Surname', with: Faker::Name.last_name
+      fill_in 'Nickname', with: Faker::Internet.user_name
       fill_in 'Password', with: '12345678'
       fill_in 'Password confirmation', with: '12345678'
       click_button('Sign up')
     end
     expect(page).to_not have_content 'Sign up'
     expect(page).to have_content 'Sign out'
-    expect(page).to have_content 'Registration successfull!'
   end
 end
